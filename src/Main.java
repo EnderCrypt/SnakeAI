22de0e2ae01ac31e184b2c169617a8a4781c10f9
Magnus <EnderCrypt@gmail.com>
2015-09-28 00:34:51 +0200

    made it possible to change screensize

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.awt.Dimension;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main
 	{
@@ -10,6 +13,7 @@ public class Main
 	public static void main(String[] args)
 		{
 		fastConsole = new BufferedOutputStream(System.out);
+		parameters(args);
 		game = new Game(screenSize);
 		while (true)
 			{
@@ -24,6 +28,31 @@ public class Main
 				System.exit(1);
 				}
 			try{Thread.sleep(50);}catch(InterruptedException et){et.printStackTrace();}
+			}
+		}
+	public static void parameters(String[] args)
+		{
+		// load all args
+		List<String> list = Arrays.asList(args);
+		Iterator<String> itr = list.iterator();
+		while (itr.hasNext())
+			{
+			String input = itr.next().toLowerCase();
+			switch(input)
+				{
+				case "-s": //change screen size
+					screenSize = new Dimension(Integer.parseInt(itr.next()),Integer.parseInt(itr.next())); 
+				break;
+				case "-h": //get help
+				case "--h":
+				case "-help":
+				case "--help":
+				default:
+					System.out.println("-h help");
+					System.out.println("	lists all the help\n");
+					System.exit(0);
+				break;
+				}
 			}
 		}
 	}
